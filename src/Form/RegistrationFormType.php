c3fa97af2984a68b880e217737ba916207a91d23
Loobixx <148224885+Loobixx@users.noreply.github.com>
2025-12-13 16:15:37 +0100

    Amélioration de l'affichage de la barre de navigation, de la page connexion, de la page inscription et amélioration de la sécurité du mot de passe

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,12 +5,14 @@ namespace App\Form;
 use App\Entity\Personne;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
 
 class RegistrationFormType extends AbstractType
 {
@@ -18,6 +20,7 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,17 +29,37 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'required' => true,
+
+                'first_options'  => [
+                    'label' => 'Mot de passe',
+
+                    // 1. Le message d'aide qui s'affichera sous le champ
+                    'help' => 'Votre mot de passe doit contenir au moins 12 caractères, comprenant des majuscules, minuscules, chiffres et caractères spéciaux.',
+
+                    // 2. Attributs HTML5 pour aider le navigateur (validation client)
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'placeholder' => 'Ex: J\'aime_Symfony_6!',
+                        'minlength' => 12, // Bloque la validation si < 12 caractères
+                    ],
+                ],
+
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ],
+
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        'max' => 4096,
+                    new NotBlank(['message' => 'Veuillez entrer un mot de passe']),
+                    new PasswordStrength([
+                        'minScore' => PasswordStrength::STRENGTH_STRONG,
+                        'message' => 'Le mot de passe est trop faible. Veuillez suivre les instructions ci-dessous.',
                     ]),
                 ],
             ])
